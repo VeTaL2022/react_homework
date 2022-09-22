afb813ad17986b6bdc30620ad0127a1104f1c316
VeTaL2022 <vetal.kots2003@gmail.com>
2022-09-22 12:07:25 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 // eslint-disable-next-line no-unused-vars
 import Users from "./components/Users/Users";
+// eslint-disable-next-line no-unused-vars
 import Comments from "./components/Comments/Comments";
 
 function App() {
   return (
     <div>
       {/*{<Users/>}*/}
-      {<Comments/>}
+      {/*{<Comments/>}*/}
     </div>
   );
 }
